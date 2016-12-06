10a47cfe5e34351cb01ada440738dc7d950bf6ed
ktsiounis <konstantinos.tsiounis@hotmail.com>
2016-12-06 20:11:26 +0200

    Added the files for the first workshop

---
+++ SimpleCalculator/app/src/main/java/com/example/konstantinos/simplecalculator/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.konstantinos.simplecalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText firstNum;
+    EditText secondNum;
+    TextView total;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firstNum = (EditText) findViewById(R.id.FirstNum);
+        secondNum = (EditText) findViewById(R.id.SecondNum);
+        total = (TextView) findViewById(R.id.TotalTextView);
+
+        Button plusBtn = (Button) findViewById(R.id.PlusBtn);
+        plusBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                float result = Float.parseFloat(firstNum.getText().toString()) + Float.parseFloat(secondNum.getText().toString());
+                total.setText(Float.toString(result));
+            }
+        });
+
+
+        Button multiplyBtn = (Button) findViewById(R.id.MultiplyBtn);
+        multiplyBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                float result = Float.parseFloat(firstNum.getText().toString()) * Float.parseFloat(secondNum.getText().toString());
+                total.setText(Float.toString(result));
+            }
+        });
+    }
+}
